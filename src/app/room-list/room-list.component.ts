98cfb2162780764fff8a8e4e15bb00edc2bb7319
unknown <ilijamilosevicc@gmail.com>
2023-05-23 12:45:02 +0200

    Third commit

--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -19,7 +19,7 @@ export class RoomListComponent implements OnInit {
       new Room("Babin zub", "With 4 pools, an on-site restaurant.", 299, "https://images.unsplash.com/photo-1455587734955-081b22074882?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80", 8.9),
       new Room("Hilton Beograd", "Located in city center.", 399, "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80", 9.0),
       new Room("Kalithea Suites", "Great hotel with sea view.", 199, "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80", 8.6),
-      new Room("Hotel Grand", "This luxury hotel on Kopaonik will give you best place for recreation.", 549, "https://images.unsplash.com/photo-1582719508461-905c673771fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1025&q=80", 8.6)
+      new Room("Hotel Grand Spa", "This luxury hotel on Kopaonik will give you best place for recreation.", 549, "https://images.unsplash.com/photo-1582719508461-905c673771fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1025&q=80", 8.6)
     ]
   }
 
